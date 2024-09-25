c63dd5511ec9e664796c7b67f2c1ef0d33b56872
Ganesh Shendge <ganesh.shendge90@gmail.com>
2024-09-25 11:18:31 +0530

    Data ingestion changes

--- app.py
+++ app.py
@@ -7,23 +7,17 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'src')))
 from mlproject.logger import logging
 from mlproject.exception import CustomException
 from mlproject.components.data_ingestion import DataIngestion
+#from mlproject.components.data_ingestion import DataIngestionConfig
 
-if __name__ == "__main__":
+if __name__=="__main__":
     logging.info("The execution has started")
 
     try:
-        # Initialize data ingestion
-        data_ingestion = DataIngestion()
-        train_data_path, test_data_path = data_ingestion.initiate_data_ingestion()
-
-        logging.info(f"Data ingestion completed successfully.")
-        logging.info(f"Training data stored at: {train_data_path}")
-        logging.info(f"Test data stored at: {test_data_path}")
-
-    except CustomException as e:
-        logging.error(f"An error occurred during data ingestion: {str(e)}")
-        raise e
+        #data_ingestion_config=DataIngestionConfig()
+        data_ingestion=DataIngestion()
+        data_ingestion.initiate_data_ingestion()
 
     except Exception as e:
-        logging.error(f"An unexpected error occurred: {str(e)}")
-        raise CustomException(e, sys)
+        logging.info("Custom Exception")
+        raise CustomException(e,sys)
+
--- template.py
+++ template.py
@@ -4,10 +4,9 @@ import logging
 
 logging.basicConfig(level=logging.INFO)
 
-project_name = "mlproject"
+project_name="mlproject"
 
-# List of files to create
-list_of_file = [
+list_of_file=[
     f"src/{project_name}/__init__.py",
     f"src/{project_name}/components/__init__.py",
     f"src/{project_name}/components/data_ingestion.py",
@@ -26,20 +25,17 @@ list_of_file = [
     "requirements.txt",
     "setup.py"
 ]
-
-# Iterate over the list of files
 for filepath in list_of_file:
-    filepath = Path(filepath)  # Convert to Path object
-    filedir, filename = os.path.split(filepath)  # Split into directory and file
+    filepath = Path(filepath)
+    filedir,filename = os.path.split(filepath)
 
-    # Create directories if they do not exist
-    if filedir:
-        os.makedirs(filedir, exist_ok=True)
-        logging.info(f"Creating directory: {filedir} for the file: {filename}")
+    if filedir != "":
+        os.makedirs(filedir,exist_ok=True)
+        logging.info(f"Creating directory:{filedir} for the file {filename}")
 
-    # Create the file if it doesn't exist or is empty
-    if not os.path.exists(filepath) or os.path.getsize(filepath) == 0:
-        with open(filepath, 'w') as f:
+    if(not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+        with open(filepath,'w') as f:
+            pass
             logging.info(f"Creating empty file: {filepath}")
     else:
-        logging.info(f"{filename} already exists")
+        logging.info(f"{filename} is already exists")
